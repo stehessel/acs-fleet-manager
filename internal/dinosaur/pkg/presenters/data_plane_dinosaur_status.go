8c05dc7991a44fdcccef8d63e1c663194d5d5bbc
Moritz Clasmeier <mtesseract@users.noreply.github.com>
2022-05-25 10:13:53 +0200

    Add conversion function for data plane dinosaur status (#44)
    Add conversion function for data plane dinosaur status.

--- internal/dinosaur/pkg/presenters/data_plane_dinosaur_status.go
+++ internal/dinosaur/pkg/presenters/data_plane_dinosaur_status.go
@@ -6,7 +6,37 @@ import (
 )
 
 func ConvertDataPlaneDinosaurStatus(status map[string]private.DataPlaneDinosaurStatus) []*dbapi.DataPlaneDinosaurStatus {
-	// TODO implement converter
-	var res []*dbapi.DataPlaneDinosaurStatus
+	res := make([]*dbapi.DataPlaneDinosaurStatus, len(status))
+
+	for k, v := range status {
+		c := make([]dbapi.DataPlaneDinosaurStatusCondition, len(v.Conditions))
+		var routes []dbapi.DataPlaneDinosaurRouteRequest
+		for _, s := range v.Conditions {
+			c = append(c, dbapi.DataPlaneDinosaurStatusCondition{
+				Type:    s.Type,
+				Reason:  s.Reason,
+				Status:  s.Status,
+				Message: s.Message,
+			})
+		}
+		if v.Routes != nil {
+			routes = make([]dbapi.DataPlaneDinosaurRouteRequest, len(*v.Routes))
+			for _, ro := range *v.Routes {
+				routes = append(routes, dbapi.DataPlaneDinosaurRouteRequest{
+					Name:   ro.Name,
+					Prefix: ro.Prefix,
+					Router: ro.Router,
+				})
+			}
+		}
+		res = append(res, &dbapi.DataPlaneDinosaurStatus{
+			DinosaurClusterId:       k,
+			Conditions:              c,
+			Routes:                  routes,
+			DinosaurVersion:         v.Versions.Dinosaur,
+			DinosaurOperatorVersion: v.Versions.DinosaurOperator,
+		})
+	}
+
 	return res
 }
