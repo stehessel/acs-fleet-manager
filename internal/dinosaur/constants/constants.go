21e6f0a292d1e835df76af550aa62958a08353c9
Simon Bäumer <baeumer@pm.me>
2022-05-25 16:08:40 +0200

    Add product name constant (#47)

--- internal/dinosaur/constants/constants.go
+++ internal/dinosaur/constants/constants.go
@@ -1,4 +1,8 @@
 package constants
 
-// ClusterNodeScaleIncrement - default increment/ decrement node count when scaling multiAZ clusters
-const DefaultClusterNodeScaleIncrement = 3
+const (
+	// ProductName defines the name of the product
+	ProductName = "RHACS"
+	// DefaultClusterNodeScaleIncrement - default increment/ decrement node count when scaling multiAZ clusters
+	DefaultClusterNodeScaleIncrement = 3
+)
