0a2277adf5f24af1dd9430b26174b7865c05742a
Moritz Clasmeier <mtesseract@users.noreply.github.com>
2022-08-30 12:18:37 +0200

    Delete leftover namespaces in down script (#340)
    Test Env: Delete leftover namespaces after deleting fleet-manager deployment

--- dev/env/scripts/down.sh
+++ dev/env/scripts/down.sh
@@ -14,3 +14,13 @@ port-forwarding stop db 5432 || true
 delete "${MANIFESTS_DIR}/db" || true
 delete "${MANIFESTS_DIR}/fleet-manager" || true
 delete "${MANIFESTS_DIR}/fleetshard-sync" || true
+
+central_namespaces=$($KUBECTL get namespace -o jsonpath='{range .items[?(@.status.phase == "Active")]}{.metadata.name}{"\n"}{end}' | grep '^rhacs-.*$' || true)
+
+for namespace in $central_namespaces; do
+    $KUBECTL delete namespace "$namespace" &
+done
+log "Waiting for leftover RHACS namespaces to be deleted... "
+for p in $(jobs -pr); do
+    wait "$p"
+done
